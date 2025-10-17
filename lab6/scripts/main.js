a1803e07f4c427164f2a53f1f76099ae367a6cab
Giuseppe Mazzeo <a22204542@alunos.ulht.pt>
2025-10-17 02:50:01 +0100

    lab6 - js concluido

--- lab6/scripts/main.js
+++ lab6/scripts/main.js
@@ -6,26 +6,34 @@ import { produtos } from './produtos.js';
 
 const produtos_section = document.querySelector('#produtos');
 const cesto_section = document.querySelector('#cesto');
-const h2_produtos = document.querySelector('#h2_produtos');
-const h2_cesto = document.querySelector('#h2_cesto');
+const quantProdutos_p = document.querySelector('#quantProdutos');
+let quantProduto = 0;
 
 produtos.forEach(function(produto) {
-    
+    criarElementosHMTML(produtos_section, produto.title, produto.image, produto.category, produto.price, produto.description, produto.rating.rate, produto.rating.count);
+
+    quantProdutos_p.textContent = ++quantProduto;
 });
 
-for (let i = 0; i<3; i++) {
-    criarElementosHMTML(produtos[i].title, produtos[i].image, produtos[i].category, produtos[i].price, produtos[i].description);
-}
+// for (let i = 0; i<10; i++) {
+//     criarElementosHMTML(produtos_section, produtos[i].title, produtos[i].image, produtos[i].category, produtos[i].price, produtos[i].description);
+
+//     quantProdutos_p.textContent = ++quantProduto;
+// }
 
 
 
 // Criando e dando valores aos elementos HTML
-function criarElementosHMTML(title, image, category, price, description, id) {
+function criarElementosHMTML(noPai, title, image, category, price, description, rate, ratingCount) {
     let article = document.createElement('article');
     let img = document.createElement('img');
     let h3 = document.createElement('h3');
     let span = document.createElement('span');
     let span_2 = document.createElement('span');
+    let span_3 = document.createElement('span');
+    let span_4 = document.createElement('span');
+    let span_5 = document.createElement('span');
+    let span_6 = document.createElement('span');
     let p = document.createElement('p');
     let p_2 = document.createElement('p');
     let p_3 = document.createElement('p');
@@ -35,6 +43,9 @@ function criarElementosHMTML(title, image, category, price, description, id) {
     img.setAttribute('src', image);
     img.setAttribute('alt', category);
 
+    span_3.innerHTML = '<span class="material-symbols-outlined">hotel_class</span>' + rate;
+    span_3.classList.add('avaliacao');
+
     h3.textContent = title;
     
     span.textContent = price + ' €';
@@ -42,7 +53,11 @@ function criarElementosHMTML(title, image, category, price, description, id) {
     button_2.textContent = 'Ver mais';
     p_2.append(span, button_2);
 
-    p.textContent = description;
+    span_4.textContent = description;
+    span_6.textContent = ratingCount;
+    span_6.classList.add('contAvaliacao')
+    span_5.append('Avaliado por ', span_6, ' pessoas');
+    p.append(span_4, span_5);
     p.classList.add('descricao');
     button_2.addEventListener('click', () => {
         mostrarDescricao(window.getComputedStyle(p).display, button_2, p);
@@ -50,7 +65,7 @@ function criarElementosHMTML(title, image, category, price, description, id) {
 
     span_2.textContent = 'shopping_bag';
     span_2.classList.add('material-symbols-outlined');
-    p_3.textContent = '+';
+    p_3.textContent = (noPai.id == 'cesto') ? '-' : '+';
     button.classList.add('adicionarCesto');
     button.append(span_2, p_3);
     button.addEventListener('click', () => {
@@ -59,13 +74,13 @@ function criarElementosHMTML(title, image, category, price, description, id) {
 
     article.classList.add('produto');
 
-    criarNos(article, h3, img, p_2, p, button);
+    criarNos(noPai, article, h3, img, p_2, p, button, span_3);
 }
 
 // Criando os nós pai e filho
-function criarNos(article, h3, img, p_2, p, button) {
-    article.append(img, h3, p_2, p, button);
-    produtos_section.append(article);
+function criarNos(noPai, article, h3, img, p_2, p, button, span_3) {
+    article.append(img, span_3, h3, p_2, p, button);
+    noPai.append(article);
 }
 
 
@@ -91,28 +106,61 @@ function mostrarDescricao(toggleStatus, button, p) {
     }   
 }
 
+
+
+
+
+
+
+
+
+
+
+
+// ========================================================================
+// Adicionar no cesto
+// ========================================================================
+
+const h2_cesto = document.querySelector('#h2_cesto');
+const custoTotal_p = document.querySelector('#custoTotal');
+const quantCesto_p = document.querySelector('#quantCesto');
+let quantCesto = 0;
+let custoTotal = 0;
+
 function adionarNoCesto(article, p) {
     if (p.textContent == '+') {
-        cesto_section.append(article);
-        p.textContent = '-';
+        let title = article.querySelector('h3').textContent;
+        let image = article.querySelector('img').src;
+        let category = article.querySelector('img').alt;
+        let price = article.querySelector('.preco').textContent.slice(0, -2);
+        let description = article.querySelector('.descricao').textContent;
+        let rate = article.querySelector('.avaliacao').textContent.slice(-3);
+        let ratingCount = article.querySelector('.contAvaliacao').textContent;
+
+        custoTotal += (parseFloat(price));
+        quantCesto++;
+
+        criarElementosHMTML(cesto_section, title, image, category, price, description, rate, ratingCount);
     } else if (p.textContent == '-') {
-        produtos_section.append(article);
-        p.textContent = '+';
+        custoTotal -= article.querySelector('.preco').textContent.slice(0, -2);
+        article.remove();
+        quantCesto--;
+    }
+
+    quantCesto_p.textContent = quantCesto;
+
+    if (custoTotal == 0) {
+        custoTotal_p.style.display = 'none';
+    } else {
+        custoTotal_p.textContent = custoTotal.toFixed(2) + ' €';
+        custoTotal_p.style.display = 'inline';
     }
 
     if (cesto_section.children.length != 0) {
-        h2_cesto.style.display = 'block';
+        h2_cesto.style.display = 'flex';
         cesto_section.style.display = 'flex';
     } else {
         h2_cesto.style.display = 'none';
         cesto_section.style.display = 'none';
-    }
-
-    if (produtos_section.children.length != 0) {
-        h2_produtos.style.display = 'block';
-        produtos_section.style.display = 'flex';
-    } else {
-        h2_produtos.style.display = 'none';
-        produtos_section.style.display = 'none';
     }
 }
