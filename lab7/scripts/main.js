6405076fdcd37f97cd03737a3087eebcbc6c4269
Giuseppe Mazzeo <a22204542@alunos.ulht.pt>
2025-10-24 00:38:22 +0100

    lab7 - finalizado

--- lab7/scripts/main.js
+++ lab7/scripts/main.js
@@ -2,8 +2,9 @@
 //              Índice
 //
 // Criar produtos
-// Criar filtros
 // Adicionar e remover no cesto
+// Criar filtros
+// Fazer a compra dos produto(s)
 // Verificar no localStorage
 //
 // * * * * * * * * * * * * * * * * * *
@@ -155,6 +156,101 @@ renderizarProdutos(produtosAPI);
 
 
 
+// ========================================================================
+// Adicionar e remover no cesto
+// ========================================================================
+
+const cestoHTML = document.querySelector('#cesto');
+const h2_cesto = document.querySelector('#h2_cesto');
+const custoTotalCesto = document.querySelector('#custoTotal');
+const quantCesto = document.querySelector('#quantCesto');
+const buttonMostrarCampoPagamento = document.querySelector('#mostrarCampoPagamento');
+
+
+function renderizarCesto(produtosNoCesto) {
+    let somatorio = 0;
+    cestoHTML.innerHTML = '';
+
+    produtosNoCesto.forEach((produto, index) => {
+        const article = criarElementosHMTML(
+            null, 
+            produto.title, 
+            produto.image, 
+            produto.category, 
+            produto.price, 
+            produto.description, 
+            produto.rate, 
+            produto.ratingCount
+        );
+
+        const p_removerCesto = article.querySelector('.adicionarCesto p');
+        const button_removerCesto = article.querySelector('.adicionarCesto');
+
+        p_removerCesto.textContent = '-';
+        button_removerCesto.onclick = () => removerNoCesto(index);
+       
+        somatorio += parseFloat(produto.price);
+        cestoHTML.append(article);
+    });
+
+    if (custoTotalCesto != 0) {
+        custoTotalCesto.style.display = 'inline';
+        custoTotalCesto.textContent = somatorio.toFixed(2) + ' €';
+    } else {
+        custoTotalCesto.style.display = 'none';
+    }
+
+    h2_cesto.style.display = (cestoHTML.children.length != 0) ? 'flex' : 'none';
+    cestoHTML.style.display = (cestoHTML.children.length != 0) ? 'flex' : 'none';
+
+    if (cestoHTML.children.length != 0) {
+        buttonMostrarCampoPagamento.classList.remove('oculto');
+    } else {
+        buttonMostrarCampoPagamento.classList.add('oculto');
+    }
+
+    quantCesto.textContent = produtosNoCesto.length;
+}
+
+function adionarNoCesto(article) {
+    const produto = {
+        title: article.querySelector('h3').textContent,
+        image: article.querySelector('img').src,
+        category: article.querySelector('img').alt,
+        price: article.querySelector('.preco').textContent.slice(0, -2),
+        description: article.querySelector('.descricao').textContent,
+        rate: article.querySelector('.avaliacao').textContent.slice(-3),
+        ratingCount: article.querySelector('.contAvaliacao').textContent
+    }
+
+    let produtosNoCesto = JSON.parse(localStorage.getItem('produtosNoCesto')) || [];
+
+    produtosNoCesto = [...produtosNoCesto, produto];
+
+    localStorage.setItem('produtosNoCesto', JSON.stringify(produtosNoCesto));
+
+    renderizarCesto(produtosNoCesto);
+}
+
+function removerNoCesto(index) {
+    let produtosNoCesto = JSON.parse(localStorage.getItem('produtosNoCesto'));
+
+    produtosNoCesto.splice(index, 1);
+
+    localStorage.setItem('produtosNoCesto', JSON.stringify(produtosNoCesto));
+
+    renderizarCesto(produtosNoCesto);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 // ========================================================================
@@ -258,86 +354,105 @@ filtroBusca.addEventListener('keyup', () => { filtrarPorNome() });
 
 
 
-
 // ========================================================================
-// Adicionar e remover no cesto
+// Fazer a compra dos produto(s)
 // ========================================================================
 
-const cestoHTML = document.querySelector('#cesto');
-const h2_cesto = document.querySelector('#h2_cesto');
-const custoTotalCesto = document.querySelector('#custoTotal');
-const quantCesto = document.querySelector('#quantCesto');
+const campoPagamento = document.querySelector('#campoPagamento');
+const formPagamento = document.querySelector('#formPagamento');
+const buttonPagamento = document.querySelector('#formPagamento > button');
+const buttonFecharCampoPagamento = document.querySelector('#fecharCampoPagamento');
+const estudante = document.querySelector('#estudante');
+const codigoCupom = document.querySelector('#cupom');
+const nomePagamento = document.querySelector('#nome');
+const mensagensCompraRealizada = document.querySelector('#mensagensCompraRealizada');
+const mensagemNome = document.querySelector('#mensagemNome');
+const mensagemEndereço = document.querySelector('#mensagemEndereço');
+const mensagemPrecoFinal = document.querySelector('#mensagemPrecoFinal');
+const mensagemReferencia = document.querySelector('#mensagemReferencia');
+let eEstudante = false;
 
 
 
-function renderizarCesto(produtosNoCesto) {
-    let somatorio = 0;
-    cestoHTML.innerHTML = '';
+function retornaIDsProdutosCesto() {
+    let ids = [];
 
-    produtosNoCesto.forEach((produto, index) => {
-        const article = criarElementosHMTML(
-            null, 
-            produto.title, 
-            produto.image, 
-            produto.category, 
-            produto.price, 
-            produto.description, 
-            produto.rate, 
-            produto.ratingCount
-        );
-
-        const p_removerCesto = article.querySelector('.adicionarCesto p');
-        const button_removerCesto = article.querySelector('.adicionarCesto');
-
-        p_removerCesto.textContent = '-';
-        button_removerCesto.onclick = () => removerNoCesto(index);
-       
-        somatorio += parseFloat(produto.price);
-        cestoHTML.append(article);
+    JSON.parse(localStorage.getItem('produtosNoCesto')).forEach((produtoNoCesto) => {
+        produtosAPI.forEach((produto) => {
+            if (produto.title == produtoNoCesto.title) {
+                ids = [... ids, produto.id];
+            }
+        });
     });
+    return ids;
+}
 
-    if (custoTotalCesto != 0) {
-        custoTotalCesto.style.display = 'inline';
-        custoTotalCesto.textContent = somatorio.toFixed(2) + ' €';
+function toggleCampoPagemento(toggleStatus) {
+    if (toggleStatus) {
+        campoPagamento.classList.remove('oculto');
     } else {
-        custoTotalCesto.style.display = 'none';
+        campoPagamento.classList.add('oculto');
+    }   
+}
+
+function textoPrecoFinal(valorFinal) {
+    let texto = 'Valor final a pagar: ';
+    let custoTotal = custoTotalCesto.textContent.slice(0, -2);
+    
+    if (custoTotal > valorFinal) {
+        texto += `<span id="comDesconto">${custoTotal} €</span> `;
+    } 
+
+    texto += `<span id="valorFinal">${valorFinal} €</span>`;
+
+    console.log(texto)
+
+    return texto;
+}
+
+
+
+buttonMostrarCampoPagamento.addEventListener('click', () => toggleCampoPagemento(true));
+
+buttonFecharCampoPagamento.addEventListener('click', () => toggleCampoPagemento(false));
+
+estudante.addEventListener('click', () => { 
+    eEstudante = !eEstudante;
+});
+
+formPagamento.addEventListener('submit', async (event) => {
+    const url_post = 'https://deisishop.pythonanywhere.com/buy/';
+    event.preventDefault();
+    
+    try {
+        const resposta = await fetch(url_post, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify ({
+                "products": retornaIDsProdutosCesto(),
+                "student": eEstudante,
+                "coupon": codigoCupom.value,
+                "name": nomePagamento.value
+            })
+        });
+
+        const dados = await resposta.json();
+
+        mensagensCompraRealizada.classList.remove('oculto');
+        mensagemNome.textContent = dados.message;
+        mensagemEndereço.textContent = dados.address;
+        mensagemPrecoFinal.innerHTML = textoPrecoFinal(dados.totalCost);
+        mensagemReferencia.innerHTML = `Referência de pagamento: <span id="referencia">${dados.reference}</span>`;
+    } catch (erro) {
+        mensagemNome.textContent = 'Lamentamos, mas houve um erro no processo da compra.';
     }
+});
 
-    h2_cesto.style.display = (cestoHTML.children.length != 0) ? 'flex' : 'none';
-    cestoHTML.style.display = (cestoHTML.children.length != 0) ? 'flex' : 'none';
 
-    quantCesto.textContent = produtosNoCesto.length;
-}
 
-function adionarNoCesto(article) {
-    const produto = {
-        title: article.querySelector('h3').textContent,
-        image: article.querySelector('img').src,
-        category: article.querySelector('img').alt,
-        price: article.querySelector('.preco').textContent.slice(0, -2),
-        description: article.querySelector('.descricao').textContent,
-        rate: article.querySelector('.avaliacao').textContent.slice(-3),
-        ratingCount: article.querySelector('.contAvaliacao').textContent
-    }
 
-    let produtosNoCesto = JSON.parse(localStorage.getItem('produtosNoCesto')) || [];
-
-    produtosNoCesto = [...produtosNoCesto, produto];
-
-    localStorage.setItem('produtosNoCesto', JSON.stringify(produtosNoCesto));
-
-    renderizarCesto(produtosNoCesto);
-}
-
-function removerNoCesto(index) {
-    let produtosNoCesto = JSON.parse(localStorage.getItem('produtosNoCesto'));
-
-    produtosNoCesto.splice(index, 1);
-
-    localStorage.setItem('produtosNoCesto', JSON.stringify(produtosNoCesto));
-
-    renderizarCesto(produtosNoCesto);
-}
 
 
 
